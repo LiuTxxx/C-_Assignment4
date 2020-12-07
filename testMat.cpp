b0c1821cba0fc4487849d332c87025815552f9d4
Liu T <11910903@mail.sustech.edu.cn>
2020-12-07 11:35:34 +0800

    second commit

--- testMat.cpp
+++ testMat.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 #include "Matrix.h"
 int main() {
-    float * f = new float[8] {1.0f, 2.0f, 3.0f, 4.0f, 1.0f, 2.0f, 3.0f, 4.0f};
-    Matrix mat("A" ,f, 8, 4, 2);
+    float * f = new float[9] {1.0f, 2.0f, 3.0f, 4.0f, 1.0f, 2.0f, 3.0f, 4.0f, 5.0f};
+    Matrix mat("A" ,f, 9, 3, 3);
     Matrix mat2(mat);
     Matrix mat3("B",2,3);
-    std::cout << mat2 << 2 * mat << mat;
+    Matrix mat4 = mat2;
+    Matrix mat5;
+    std::cin >> mat5;
+    std::cout << mat <<  mat2 << mat * mat2;
+    std::cout << mat2 * mat3 << mat2 * 2.0;
+    std::cout << 2.0 * mat << mat + 2 << mat2 - 2 << mat2 / 2 << mat / 0;
+    std::cout << mat2 + mat2 << mat + mat3 << mat - mat2;
 }
